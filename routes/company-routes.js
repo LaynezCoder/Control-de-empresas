c629f69303f303665326f90ca0b064bad6133daa
LaynezCoder <vlaynez2016@gmail.com>
2021-02-20 13:25:51 -0600

    Added files: created routes

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -6,5 +6,9 @@ const api = express.Router();
 
 api.post('/login', companyController.login);
 api.post('/saveCompany', companyController.saveCompany);
+api.put('/updateCompany/:id', companyController.updateCompany);
+api.delete('/deleteCompany/:id', companyController.deleteCompany);
+api.get('/getCompanies', companyController.getCompanies);
+api.post('/getCompany/:id', companyController.getCompany);
 
 module.exports = api;
