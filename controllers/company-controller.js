b8681830c46d3c8b1580d8af26f073e561a6e806
LaynezCoder <vlaynez2016@gmail.com>
2021-02-20 13:25:31 -0600

    File added: created functions of Companies

--- controllers/company-controller.js
+++ controllers/company-controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Company = require('../models/company-models');
+const Employee = require('../models/employee-model');
 const bcrypt = require('bcrypt-nodejs');
 
 const ADMINISTRADOR = "ADMINISTRATOR";
@@ -43,6 +44,38 @@ function createAdministrator(req, res) {
     })
 }
 
+function login(req, res) {
+    let params = req.body;
+
+    if (params.username && params.password) {
+        Company.findOne({ username: params.username }, (err, userFind) => {
+            if (err) {
+                res.status(500).send({ message: 'Server error, trying to search!' })
+            } else if (userFind) {
+                bcrypt.compare(params.password, userFind.password, (err, checkPassword) => {
+                    if (err) {
+                        res.status(500).send({ message: 'General server error!' });
+                    } else if (checkPassword) {
+                        if (userFind.role === ADMINISTRADOR) {
+                            role = ADMINISTRADOR
+                        } else {
+                            role = COMPANY
+                        }
+                        console.log('User find role:', userFind.role, '|', 'Role variable: ', role, '|', 'Username', userFind.username)
+                        res.status(200).send({ message: 'User successfully logged in!' });
+                    } else {
+                        res.status(404).send({ message: 'Incorrect username or password!' })
+                    }
+                })
+            } else {
+                res.status(200).send({ message: 'Account not found!' })
+            }
+        })
+    } else {
+        res.status(200).send({ message: 'Please, enter all fields!' })
+    }
+}
+
 function saveCompany(req, res) {
     let company = new Company();
     let params = req.body;
@@ -83,40 +116,157 @@ function saveCompany(req, res) {
     }
 }
 
-function login(req, res) {
-    let params = req.body;
+function updateCompany(req, res) {
+    let userId = req.params.id;
+    let update = req.body;
 
-    if (params.username && params.password) {
-        Company.findOne({ username: params.username }, (err, userFind) => {
-            if (err) {
-                res.status(500).send({ message: 'Server error, trying to search!' })
-            } else if (userFind) {
-                bcrypt.compare(params.password, userFind.password, (err, checkPassword) => {
+    if (role === ADMINISTRADOR) {
+        if (update.password) {
+            res.status(500).send({ message: 'You cannot update the password!' });
+        } else if (update.role) {
+            res.status(500).send({ message: 'You cannot update the role!' });
+        } else {
+            if (update.name && update.username) {
+                Company.findOne({ username: update.username }, (err, usernameFind) => {
                     if (err) {
-                        res.status(500).send({ message: 'General server error!' });
-                    } else if (checkPassword) {
-                        if (userFind.role === ADMINISTRADOR) {
-                            role = ADMINISTRADOR
-                        } else {
-                            role = COMPANY
-                        }
-                        console.log('User find role:', userFind.role, '-', 'Role variable: ', role)
-                        res.status(200).send({ message: 'User successfully logged in!' });
+                        res.status(500).send({ message: 'Server error!' });
+                    } else if (usernameFind) {
+                        res.status(200).send({ message: 'Username already existing, your account cannot be updated!' });
                     } else {
-                        res.status(404).send({ message: 'Incorrect username or password!' })
+                        Company.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+                            if (err) {
+                                res.status(500).send({ message: 'Server error trying to update!' });
+                            } else if (userUpdated) {
+                                res.status(200).send({ message: 'Updated user!', userUpdated });
+                            } else {
+                                res.status(200).send({ message: 'There are no records to update!' });
+                            }
+                        });
                     }
                 })
             } else {
-                res.status(200).send({ message: 'Account not found!' })
+                res.status(200).send({ message: 'Please enter all the required data!' })
+            }
+        }
+    } else {
+        res.status(200).send({ message: 'No tiene persmisos!' })
+    }
+}
+
+function deleteCompany(req, res) {
+    let userId = req.params.id;
+
+    if (role === ADMINISTRADOR) {
+        Company.findByIdAndRemove(userId, (err, userRemoved) => {
+            if (err) {
+                res.status(500).send({ message: 'Error en el servidor al intentar eliminar el registro' });
+            } else if (userRemoved) {
+                res.status(200).send({ message: 'usuario eliminado', userRemoved });
+            } else {
+                res.status(200).send({ message: 'No existe el usuario, o ya fue eliminado' });
             }
         })
     } else {
-        res.status(200).send({ message: 'Please, enter all fields!' })
+        res.status(200).send({ message: 'No tiene persmisos!' })
+    }
+}
+
+function getCompanies(req, res) {
+    if (role === ADMINISTRADOR) {
+        Company.find({}).exec((err, users) => { //Busqueda general (filtraciones)
+            if (err) {
+                res.status(500).send({ message: 'Error en el servidor al intentar buscar' })
+            } else if (users) {
+                res.status(200).send({ message: 'usuarios encontrados', users })
+            } else {
+                res.status(200).send({ message: 'No hay registros' })
+            }
+        })
+    } else {
+        res.status(200).send({ message: 'No tiene persmisos!' })
+    }
+}
+
+function getCompany(req, res) {
+    let userId = req.params.id;
+
+    if (role === ADMINISTRADOR) {
+        Company.findById(userId).exec((err, user) => {
+            if (err) {
+                res.status(500).send({ message: 'Error en el servidor al intentar buscar' });
+            } else if (user) {
+                res.status(200).send({ message: 'usuario encontrado', user });
+            } else {
+                res.status(200).send({ message: 'No hay registros' });
+            }
+        })
+    } else {
+        res.status(200).send({ message: 'No tiene persmisos!' })
+    }
+}
+
+function createEmployee(req, res) {
+    let userId = req.params.id;
+    let params = req.body;
+    let course = new Course();
+
+    console.log(session, TEACHER_ROL)
+    console.log(userId, idStudent)
+    if (session === TEACHER_ROL) {
+        if (userId == idStudent) {
+            User.findById(userId, (err, userFind) => {
+                if (err) {
+                    res.status(500).send({ message: 'Server error trying to add a course!' });
+                } else if (userFind) {
+                    Course.findOne({ name: params.name }, (err, courseFind) => {
+                        if (err) {
+                            res.status(500).send({ message: 'Server error!' });
+                        } else if (courseFind) {
+                            res.status(200).send({ message: 'Course already existing!' });
+                        } else {
+                            if (params.name && params.description) {
+                                course.name = params.name;
+                                course.description = params.description;
+
+                                User.findByIdAndUpdate(userId, { $push: { courses: course } }, { new: true }, (err, userUpdated) => {
+                                    if (err) {
+                                        res.status(500).send({ message: 'General server error!' });
+                                    } else if (userUpdated) {
+                                        res.status(200).send({ message: 'Course added!', userUpdated })
+                                    } else {
+                                        res.status(404).send({ message: 'Course not added!' });
+                                    }
+                                })
+                            } else {
+                                res.status(200).send({ message: 'Enter the minimum data to add a course!' })
+                            }
+                        }
+                    })
+                } else {
+                    res.status(200).send({ message: 'User does not exist!' });
+                }
+            });
+        } else {
+            res.status(200).send({ message: 'You cannot added a course that is not yours!' });
+        }
+    } else {
+        res.status(404).send({ message: 'You dont have permissions!' });
     }
 }
 
 module.exports = {
+    /**
+     * Company exports
+     */
     createAdministrator,
     saveCompany,
-    login
+    login,
+    updateCompany,
+    deleteCompany,
+    getCompanies,
+    getCompany,
+    /**
+     * Employee exports
+     */
+    createEmployee
 }
