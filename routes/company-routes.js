c63795ba763d2f293d9bd3476d98bacb00ddf699
LaynezCoder <vlaynez2016@gmail.com>
2021-02-20 21:54:11 -0600

    Modified files: company routes

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -20,5 +20,15 @@ api.post('/getCompany/:id', companyController.getCompany);
  */
 api.put('/:id/createEmployee', companyController.createEmployee);
 api.get('/:id/getEmployees', companyController.getEmployees);
+api.put('/:idC/updateEmployee/:idE', companyController.updatedEmployee);
+api.put('/:idC/deleteEmployee/:idE', companyController.deleteEmployee);
+
+/**
+ * Employees find
+ */
+api.get('/:idC/getEmployeeForId/:idE', companyController.getEmployeesForId);
+api.get('/:idC/getEmployeeForName', companyController.getEmployeeForName);
+api.get('/getEmployeeForJob', companyController.getEmployeeForJob);
+api.get('/getEmployeeForDepartament', companyController.getEmployeeForDepartament);
 
 module.exports = api;
