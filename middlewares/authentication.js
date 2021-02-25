1bdee95b3f6824d8864f29f7df3f0c7996eb1195
LaynezCoder <vlaynez2016@gmail.com>
2021-02-25 14:01:00 -0600

    Modified files: middleware.js

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -9,16 +9,16 @@ const SECRET_KEY = 'Shhhh'
 
 exports.ensureAuth = (req, res, next) => {
     if (!req.headers.authorization) {
-        return res.status(403).send({ message: 'La petición no lleva cabecera de autenticación' })
+        return res.status(403).send({ message: 'The request does not have an authentication header!' })
     } else {
-        let token = req.headers.authorization.replace(/['"']+/g, '');
+        var token = req.headers.authorization.replace(/['"']+/g, '');
         try {
             var payload = jwt.decode(token, SECRET_KEY);
             if (payload.exp <= moment().unix()) {
-                return res.status(401).send({ message: 'Token ha expirado' })
+                return res.status(401).send({ message: 'Token has expired' })
             }
         } catch (err) {
-            return res.status(404).send({ message: 'Token inválido' })
+            return res.status(404).send({ message: 'Invalid Token' })
         }
 
         req.user = payload;
@@ -30,7 +30,7 @@ exports.ensureAuthAdministrator = (req, res, next) => {
     let payload = req.user;
 
     if (payload.role != ADMINISTRADOR) {
-        return res.status(404).send({ message: 'No tienes permiso para ingresar a esta ruta' })
+        return res.status(404).send({ message: 'You do not have permission to enter this route!' })
     } else {
         return next();
     }
@@ -40,7 +40,7 @@ exports.ensureAuthCompany = (req, res, next) => {
     let payload = req.user;
 
     if (payload.role != COMPANY) {
-        return res.status(404).send({ message: 'No tienes permiso para ingresar a esta ruta' })
+        return res.status(404).send({ message: 'You do not have permission to enter this route!' })
     } else {
         return next();
     }
