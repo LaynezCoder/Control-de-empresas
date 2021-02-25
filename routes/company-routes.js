15cf91b34435014d859c5d8fed45e591d28d0bfd
LaynezCoder <vlaynez2016@gmail.com>
2021-02-25 06:47:33 -0600

    Added function: Authentication of roles and tokens on routes

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -3,20 +3,25 @@
 const express = require('express');
 const companyController = require('../controllers/company-controller');
 const api = express.Router();
+var mdAuth = require('../middlewares/authentication');
 
 /**
- * Companies paths
+ * Companies routes
  */
 
 api.post('/login', companyController.login);
-api.post('/saveCompany', companyController.saveCompany);
+
+/**
+ * Administrator only
+ */
+api.post('/saveCompany', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], companyController.saveCompany);
 api.put('/updateCompany/:id', companyController.updateCompany);
 api.delete('/deleteCompany/:id', companyController.deleteCompany);
 api.get('/getCompanies', companyController.getCompanies);
 api.post('/getCompany/:id', companyController.getCompany);
 
 /**
- * Employees paths
+ * Employees routes
  */
 api.put('/:id/createEmployee', companyController.createEmployee);
 api.get('/:id/getEmployees', companyController.getEmployees);
