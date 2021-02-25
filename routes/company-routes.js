7351ae67be4a7a533dd479c2204ce450ee87ebb0
LaynezCoder <vlaynez2016@gmail.com>
2021-02-25 11:26:24 -0600

    Modified files: Change constants

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -1,39 +1,39 @@
 'use strict'
 
-const express = require('express');
-const companyController = require('../controllers/company-controller');
-const api = express.Router();
+const COMPANY_CONTROLLER = require('../controllers/company-controller');
+var express = require('express');
+var api = express.Router();
 var mdAuth = require('../middlewares/authentication');
 
 /**
  * Companies routes
  */
 
-api.post('/login', companyController.login);
+api.post('/login', COMPANY_CONTROLLER.login);
 
 /**
  * Administrator only
  */
-api.post('/saveCompany', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], companyController.saveCompany);
-api.put('/updateCompany/:id', companyController.updateCompany);
-api.delete('/deleteCompany/:id', companyController.deleteCompany);
-api.get('/getCompanies', companyController.getCompanies);
-api.post('/getCompany/:id', companyController.getCompany);
+api.post('/saveCompany', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.saveCompany);
+api.put('/updateCompany/:id', COMPANY_CONTROLLER.updateCompany);
+api.delete('/deleteCompany/:id', COMPANY_CONTROLLER.deleteCompany);
+api.get('/getCompanies', COMPANY_CONTROLLER.getCompanies);
+api.post('/getCompany/:id', COMPANY_CONTROLLER.getCompany);
 
 /**
  * Employees routes
  */
-api.put('/:id/createEmployee', companyController.createEmployee);
-api.get('/:id/getEmployees', companyController.getEmployees);
-api.put('/:idC/updateEmployee/:idE', companyController.updatedEmployee);
-api.put('/:idC/deleteEmployee/:idE', companyController.deleteEmployee);
+api.put('/:id/createEmployee', COMPANY_CONTROLLER.createEmployee);
+api.get('/:id/getEmployees', COMPANY_CONTROLLER.getEmployees);
+api.put('/:idC/updateEmployee/:idE', COMPANY_CONTROLLER.updatedEmployee);
+api.put('/:idC/deleteEmployee/:idE', COMPANY_CONTROLLER.deleteEmployee);
 
 /**
  * Employees find
  */
-api.get('/:idC/getEmployeeForId/:idE', companyController.getEmployeesForId);
-api.get('/:idC/getEmployeeForName', companyController.getEmployeeForName);
-api.get('/getEmployeeForJob', companyController.getEmployeeForJob);
-api.get('/getEmployeeForDepartament', companyController.getEmployeeForDepartament);
+api.get('/:idC/getEmployeeForId/:idE', COMPANY_CONTROLLER.getEmployeesForId);
+api.get('/:idC/getEmployeeForName', COMPANY_CONTROLLER.getEmployeeForName);
+api.get('/getEmployeeForJob', COMPANY_CONTROLLER.getEmployeeForJob);
+api.get('/getEmployeeForDepartament', COMPANY_CONTROLLER.getEmployeeForDepartament);
 
 module.exports = api;
