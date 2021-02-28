fcff227a0231975b0d2215d743321ca981bc3f10
LaynezCoder <vlaynez2016@gmail.com>
2021-02-28 00:07:53 -0600

    Modified files: pdf-report.js

--- pdf/pdf-report.js
+++ pdf/pdf-report.js
@@ -4,36 +4,41 @@ var ejs = require("ejs");
 var pdf = require("html-pdf");
 var path = require("path");
 var Company = require('../models/company-models');
-const FILE = './pdf/Employees.pdf';
+
+const FILE = './pdf/Employees_';
+const DATE = require('../resources/date')
+const EXTENSION = '.pdf'
+const EJS_PACKAGE = './template/'
 
 const OPTIONS = {
-    "height": "11.25in",
-    "width": "8.5in",
-    "header": {
-        "height": "20mm"
+    'height': '11.25in',
+    'width': '8.5in',
+    'header': {
+        'height': '20mm'
     },
-    "footer": {
-        "height": "20mm",
+    'footer': {
+        'height': '20mm',
     },
 };
 
 function createReport(req, res) {
-    let userId = req.params.id;
+    let companyId = req.params.id;
 
-    if (userId == req.user.sub) {
-        Company.findOne({ _id: userId }).exec((err, employees) => {
+    if (companyId == req.user.sub) {
+        Company.findOne({ _id: companyId }).exec((err, employees) => {
             if (err) {
                 res.status(500).send({ message: 'General server error!' });
             } else if (employees) {
-                ejs.renderFile(path.join('./template/', "report-template.ejs"), { employees: employees.employees }, (err, data) => {
+                ejs.renderFile(path.join(EJS_PACKAGE, "report-template.ejs"), { employees: employees.employees }, (err, data) => {
                     if (err) {
                         res.status(500).send({ message: 'Error!' });
                     } else {
-                        pdf.create(data, OPTIONS).toFile(FILE, function (err, data) {
+                        let companie = 'for_' + employees.name + '_'
+                        pdf.create(data, OPTIONS).toFile(FILE + companie + DATE.getDate() + EXTENSION, (err, data) => {
                             if (err) {
                                 res.status(500).send({ message: 'Error!' });
-                            } else {
-                                res.send({ message: 'File created successfully' });
+                            } else if (data) {
+                                res.send({ message: 'Report created at: ' + DATE.getDateAnotherFormat() })
                             }
                         });
                     }
