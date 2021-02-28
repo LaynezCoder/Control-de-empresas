565720dfdc20e10784ef5c397d901b405f26c351
LaynezCoder <vlaynez2016@gmail.com>
2021-02-28 00:10:02 -0600

    Added file: Create function for created report of data in XLSX format

---
+++ excel/excel-report.js
@@ -0,0 +1,57 @@
+'use strict'
+
+var excel = require('exceljs');
+
+var Company = require('../models/company-models');
+
+const NAME_WORKSHEET = 'Employees';
+const FILE = './excel/Employees_';
+const EXTENSION = '.xlsx'
+const DATE = require('../resources/date')
+
+function createReport(req, res) {
+    let companyId = req.params.id;
+
+    if (companyId == req.user.sub) {
+        Company.findOne({ _id: companyId }).exec((err, employees) => {
+            if (err) {
+                res.status(500).send({ message: 'General server error!' });
+            } else if (employees) {
+                var workbook = new excel.Workbook();
+                var worksheet = workbook.addWorksheet(NAME_WORKSHEET);
+
+                worksheet.columns = [
+                    { header: 'Id', key: '_id', width: 30 },
+                    { header: 'Name', key: 'name', width: 30 },
+                    { header: 'Job', key: 'job', width: 30 },
+                    { header: 'Departament', key: 'departament', width: 30 }
+                ];
+
+                worksheet.getRow(1).font = { bold: true }
+                worksheet.getRow(1).alignment = { horizontal: 'center' }
+
+                const columns = ['A', 'B', 'C', 'D']
+                columns.forEach((c) => {
+                    worksheet.getColumn(c).alignment = { horizontal: 'center' }
+                })
+
+                worksheet.addRows(employees.employees);
+
+                let companie = 'for_' + employees.name + '_'
+                workbook.xlsx.writeFile(FILE + companie + DATE.getDate() + EXTENSION)
+                    .then(() => {
+                        res.send({ message: 'Report created at: ' + DATE.getDateAnotherFormat() })
+                    })
+
+            } else {
+                res.status(404).send({ message: 'Employees not found!' });
+            }
+        })
+    } else {
+        res.send({ message: 'You cannot view a employees that is not yours!' });
+    }
+}
+
+module.exports = {
+    createReport
+}
