af12b5aed707087ab54a5bcc8619a395b1130009
LaynezCoder <vlaynez2016@gmail.com>
2021-02-28 11:30:45 -0600

    Modified files: date.js

--- resources/date.js
+++ resources/date.js
@@ -3,11 +3,11 @@
 function getDate() {
     var date = new Date();
     var dateString =
-        ("00" + (date.getMonth() + 1)).slice(-2) + '_' +
-        ("00" + date.getDate()).slice(-2) + '_' +
-        date.getFullYear() + " " +
-        ("00" + date.getHours()).slice(-2) + '_' +
-        ("00" + date.getMinutes()).slice(-2) + '_' +
+        ("00" + (date.getMonth() + 1)).slice(-2) + '-' +
+        ("00" + date.getDate()).slice(-2) + '-' +
+        date.getFullYear() + "T" +
+        ("00" + date.getHours()).slice(-2) + '-' +
+        ("00" + date.getMinutes()).slice(-2) + '-' +
         ("00" + date.getSeconds()).slice(-2);
     return dateString
 }
