070ad7b917834d51b64776ac8309589632275acf
LaynezCoder <vlaynez2016@gmail.com>
2021-02-27 20:01:16 -0600

    Modified files: Create pdf route

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -1,9 +1,12 @@
 'use strict'
 
 const COMPANY_CONTROLLER = require('../controllers/company-controller');
+const PDF = require('../pdf/pdf-report');
+
 var express = require('express');
 var api = express.Router();
-var mdAuth = require('../middlewares/authentication');
+
+const MD_AUTH = require('../middlewares/authentication');
 
 /**
  * Companies routes
@@ -13,27 +16,34 @@ api.post('/login', COMPANY_CONTROLLER.login);
 /**
  * Administrator only
  */
-api.post('/saveCompany', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.saveCompany);
-api.put('/updateCompany/:id', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.updateCompany);
-api.delete('/deleteCompany/:id', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.deleteCompany);
-api.get('/getCompanies', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.getCompanies);
-api.post('/getCompany/:id', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.getCompany);
-api.post('/searchCompany', [mdAuth.ensureAuth, mdAuth.ensureAuthAdministrator], COMPANY_CONTROLLER.searchCompany);
+api.post('/saveCompany', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.saveCompany);
+api.put('/updateCompany/:id', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.updateCompany);
+api.delete('/deleteCompany/:id', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.deleteCompany);
+api.get('/getCompanies', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.getCompanies);
+api.post('/getCompany/:id', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.getCompany);
+api.post('/searchCompany', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthAdministrator], COMPANY_CONTROLLER.searchCompany);
 
 /**
  * Employees routes
  */
-api.put('/:id/createEmployee', [mdAuth.ensureAuth, mdAuth.ensureAuthCompany], COMPANY_CONTROLLER.createEmployee);
-api.get('/:id/getEmployees', [mdAuth.ensureAuth, mdAuth.ensureAuthCompany], COMPANY_CONTROLLER.getEmployees);
-api.put('/:idC/updateEmployee/:idE', COMPANY_CONTROLLER.updatedEmployee);
-api.put('/:idC/deleteEmployee/:idE', COMPANY_CONTROLLER.deleteEmployee);
+api.put('/:id/createEmployee', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthCompany], COMPANY_CONTROLLER.createEmployee);
+api.get('/:id/getEmployees', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthCompany], COMPANY_CONTROLLER.getEmployees);
+api.put('/:idC/updateEmployee/:idE', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthCompany], COMPANY_CONTROLLER.updatedEmployee);
+api.put('/:idC/deleteEmployee/:idE', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthCompany], COMPANY_CONTROLLER.deleteEmployee);
+api.get('/:idC/getEmployeeForId/:idE', [MD_AUTH.ensureAuth, MD_AUTH.ensureAuthCompany], COMPANY_CONTROLLER.getEmployeesForId);
+
+/**
+ * Reports
+ */
+api.get('/createReport/:id', PDF.createReport)
+
 
 /**
  * Employees find
- */
-api.get('/:idC/getEmployeeForId/:idE', COMPANY_CONTROLLER.getEmployeesForId);
+
 api.get('/:idC/getEmployeeForName', COMPANY_CONTROLLER.getEmployeeForName);
 api.get('/getEmployeeForJob', COMPANY_CONTROLLER.getEmployeeForJob);
 api.get('/getEmployeeForDepartament', COMPANY_CONTROLLER.getEmployeeForDepartament);
+ */
 
 module.exports = api;
